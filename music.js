b61940c173b5262b82b378c07c53de2f8b51701c
superbuggy <jpqreichard@gmail.com>
2023-11-19 14:26:45 -0500

    ✨ Calculate cents from hz distance from A440

--- music.js
+++ music.js
@@ -1,14 +1,18 @@
 const { log } = Math;
 import { uniques } from "./general";
 import { nPowersOf, primes } from "./math";
+const A440 = 440;
 
 export const ratioToCents = (numerator, denominator) => (1200 * log(numerator / denominator)) / log(2);
 
-const octaveReducedPrimes = () =>
+// Helper for harmonic series inquiries
+export const octaveReducedPrimes = () =>
   uniques(
     [...primes(101)].slice(1, -1).map((num) => {
       const powersOf2 = [nPowersOf(8, 2)];
       const denominator = powersOf2.find((power) => num / power < 2 && num / power > 1);
-      return [num, denominator]
+      return [num, denominator];
     })
   );
+
+export const hzToCents = (hz) => 1200 * Math.log2(hz / A440);
